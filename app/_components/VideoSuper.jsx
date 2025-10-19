019ec1a7ec9167aeb4122b0c662c9394d84b5d7b
amanqureshi454 <qureshiamanqureshi@gmail.com>
2025-10-19 23:53:56 +0530

    added

--- app/_components/VideoSuper.jsx
+++ app/_components/VideoSuper.jsx
@@ -4,6 +4,7 @@ import { Canvas, useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 import dynamic from "next/dynamic";
+import gsap from "gsap";
 
 const loadGSAP = async () => {
   try {
